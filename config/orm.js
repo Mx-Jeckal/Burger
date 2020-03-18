4c1af69d372268b57452a8db90915d67d6977bf9
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-03-18 05:44:26 -0500

    styling

--- config/orm.js
+++ config/orm.js
@@ -11,9 +11,31 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
-// `selectAll()` *
-// `insertOne()` *
-// `updateOne()`
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+
+//   The Almighty ORM
 var orm = {
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -46,7 +68,7 @@ var orm = {
 
     updateOne: (table, objectColumnValues, condition, callback) => {
         // DECLARING QUERY FOR UPDATE SQL
-        var queryString = `UPDATE ${table} SET ${objectToSQL(objectColumnValues)} WHERE ${condition};`;
+        var queryString = `UPDATE ${table} SET ${objToSql(objectColumnValues)} WHERE ${condition};`;
         connection.query(queryString, (error, result) => {
             if (error) throw (error);
             callback(result);
--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,4 +1,29 @@
 $(function() {
+
+    $(".change-devoured-state").on("click", function(event) {
+        var id = $(this).data("id");
+        var devoured = $(this).data("devoured");
+        console.log(id, devoured);
+
+        var newDevouredState = {
+            devoured: devoured
+        };
+
+        // SEND THE PUT REQUEST
+        $.ajax(`/api/burgers/${id}`, {
+            type: 'PUT',
+            data: newDevouredState
+        }).then(
+            function() {
+                console.log(`CHANGED DEVOURED TO: ${newDevouredState}`);
+                // RELOAD THE PAGE TO GET THE UPDATED LIST FROM DB
+                location.reload();
+            }
+        );
+    });
+
+
+
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
