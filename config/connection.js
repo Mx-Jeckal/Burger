5ac6a21dca6259553f5b9f4e1bb4fa3f578db3f5
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-03-18 12:23:30 -0500

    obsecured pw

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
+var password = require("./pw/pw")
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password1",
+    password: password(),
     database: "burgers_db"
 });
 
